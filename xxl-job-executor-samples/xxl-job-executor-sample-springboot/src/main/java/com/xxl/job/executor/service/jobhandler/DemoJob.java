9c22e708d71e71b6b6b50c05e5991d5896cd32da
wangzhixiang <wangzhixiang_v@didichuxing.com>
2020-08-22 10:37:23 +0800

    优化

---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/DemoJob.java
@@ -0,0 +1,10 @@
+package com.xxl.job.executor.service.jobhandler;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by wangzhixiang on 2020/8/20.
+ */
+@Component
+public class DemoJob {
+}
